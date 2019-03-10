f71bc371b7a5034b0a3e6611cb59abef10161fbb
dgp <dgp-web@outlook.de>
2019-03-10 18:44:46 +0100

    Add entity-action-types.model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ export * from "./composite-entity-action-config.model";
 export * from "./create-entity-reducer-config.model";
 export * from "./entity.model";
 export * from "./entity-action-params.model";
+export * from "./entity-action-types.model";
 export * from "./entity-reducer.model";
 export * from "./entity-state.model";
 export * from "./key-value-store.model";
