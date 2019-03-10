14e2e193472d8f66ffe471d96d812e9c9145d915
dgp <dgp-web@outlook.de>
2019-03-10 18:51:58 +0100

    Implement createEntityActionTypes

---
+++ src/functions/create-entity-action-types.function.ts
@@ -0,0 +1,23 @@
+import { CompositeEntityActionConfig, defaultCompositeEntityActionConfig, EntityActionTypes } from "../models";
+import { composeEntityActionType } from "./compose-entity-action-type.function";
+
+export interface CreateEntityActionTypesPayload {
+    readonly entityType: string;
+    readonly storeFeature?: string;
+}
+
+export function createEntityActionTypes(
+    payload: CreateEntityActionTypesPayload,
+    config: CompositeEntityActionConfig = defaultCompositeEntityActionConfig
+): EntityActionTypes {
+
+    return {
+      addEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.add, payload.storeFeature),
+      updateEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.update, payload.storeFeature),
+      removeEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.remove, payload.storeFeature),
+      clearEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.clear, payload.storeFeature),
+      setEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.set, payload.storeFeature),
+      selectEntityActionType: composeEntityActionType(payload.entityType, config.prefixes.select, payload.storeFeature)
+    };
+
+}
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,6 @@
 export * from "./compose-entity-action-type.function";
 export * from "./create-action-type-segments-for-suffix.function";
+export * from "./create-entity-action-types.function";
 export * from "./create-entity-reducer.function";
 export * from "./create-entity-state.function";
 export * from "./create-kvs-from-array.function";
