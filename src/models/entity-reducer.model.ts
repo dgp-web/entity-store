03b34b1181920194b782092c56f93ba5e47716d3
dgp <dgp-web@outlook.de>
2019-03-12 15:06:58 +0100

    Add reducer.model and use it in entity-reducer

--- src/models/entity-reducer.model.ts
+++ src/models/entity-reducer.model.ts
@@ -1,8 +1,7 @@
 import { EntityState } from "./entity-state.model";
-import { Action } from "./action.model";
+import { Reducer } from "./reducer.model";
 
 /**
  * Signature of an entity reducer
  */
-export type EntityReducer<TModel, TState extends EntityState<TModel> & TAttributes, TAttributes> =
-    (state: TState, action: Action) => TState;
+export type EntityReducer<TModel, TState extends EntityState<TModel> & TAttributes, TAttributes> = Reducer<TState>;
