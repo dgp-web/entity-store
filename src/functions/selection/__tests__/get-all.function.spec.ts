3ea476b850a83bb6f259493a54939bd4620e494d
dgp <dgp-web@outlook.de>
2019-03-14 13:16:29 +0100

    Add test stubs for selection functions

---
+++ src/functions/selection/__tests__/get-all.function.spec.ts
@@ -0,0 +1,7 @@
+describe("getAll", () => {
+
+    xit("should return all entities included in an entity state, sorted after their position in ids.", () => {
+
+    });
+
+});
---
+++ src/functions/selection/__tests__/get-first-selected.function.spec.ts
@@ -0,0 +1,7 @@
+describe("getFirstSelected", () => {
+
+    xit("should return the entities associated with the first id in selectedIds.", () => {
+
+    });
+
+});
