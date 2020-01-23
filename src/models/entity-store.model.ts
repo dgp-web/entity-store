4bec70768199eaf975234b6ae41eb40d53cd3b18
dgp <dgp-web@outlook.de>
2020-01-23 20:05:19 +0100

    Extract EntityStoreActions type from entity-store.model

--- src/models/entity-store.model.ts
+++ src/models/entity-store.model.ts
@@ -1,18 +1,21 @@
-import {EntityReducerMap} from "./entity-reducer-map.model";
-import {EntityStateMap} from "./entity-state-map.model";
-import {EntityTypeMap} from "./entity-type-map.model";
-import {CompositeEntityActionPayload} from "./composite-entity-action-payload.model";
-import {CompositeEntityActionConfig} from "./composite-entity-action-config.model";
-import {ComposedEntityActions} from "./composed-entity-actions.model";
-import {EntitySelectorMap} from "./entity-selector-map.model";
+import { EntityReducerMap } from "./entity-reducer-map.model";
+import { EntityTypeMap } from "./entity-type-map.model";
+import { CompositeEntityActionPayload } from "./composite-entity-action-payload.model";
+import { CompositeEntityActionConfig } from "./composite-entity-action-config.model";
+import { ComposedEntityActions } from "./composed-entity-actions.model";
+import { EntitySelectorMap } from "./entity-selector-map.model";
 
+export interface EntityStoreActions<TEntityTypeMap extends EntityTypeMap, TStoreFeature> {
+    composeEntityActions(
+        payload: CompositeEntityActionPayload<TEntityTypeMap, TStoreFeature>, config?: CompositeEntityActionConfig
+    ): ComposedEntityActions<TEntityTypeMap, TStoreFeature>;
+}
+
+/**
+ * Object that exposes matching actions, reducers, and selectors
+ */
 export interface EntityStore<TEntityTypeMap extends EntityTypeMap, TStoreFeature = string> {
-    readonly reducers: EntityReducerMap<EntityStateMap<TEntityTypeMap>, TEntityTypeMap>;
-    readonly actions: {
-        composeEntityActions: (
-            payload: CompositeEntityActionPayload<TEntityTypeMap, TStoreFeature>,
-            config?: CompositeEntityActionConfig
-        ) => ComposedEntityActions<TEntityTypeMap, TStoreFeature>
-    };
+    readonly reducers: EntityReducerMap<TEntityTypeMap>;
+    readonly actions: EntityStoreActions<TEntityTypeMap, TStoreFeature>;
     readonly selectors: EntitySelectorMap<TEntityTypeMap>;
 }
