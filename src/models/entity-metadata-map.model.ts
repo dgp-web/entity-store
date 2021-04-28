76076061aec663d4268706dbbb637984b75c2267
dgp <dgp-web@outlook.de>
2021-04-28 13:46:28 +0200

    1.6.0 Add entity metadata map

---
+++ src/models/entity-metadata-map.model.ts
@@ -0,0 +1,6 @@
+import { EntityTypeMap } from "./entity-type-map.model";
+import { EntityMetadata } from "./entity-metadata.model";
+
+export type EntityMetadataMap<TEntityTypeMap extends EntityTypeMap> = {
+    readonly [K in keyof TEntityTypeMap]: EntityMetadata<TEntityTypeMap[K]>;
+}
