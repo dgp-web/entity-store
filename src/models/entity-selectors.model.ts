f0cc40c138fd68cdc2462ed0b6bdd6398b3befdd
dgp-web <dgp-web@outlook.de>
2022-03-30 08:23:22 +0200

    2.0.0 Upgrade to new data-modeling

--- src/models/entity-selectors.model.ts
+++ src/models/entity-selectors.model.ts
@@ -1,10 +1,10 @@
 import { Selector } from "./selector.model";
-import { EntityStateMap, EntityTypeMap, KVS } from "data-modeling";
+import { EntityStateMap, EntityTypeMap, KVS, Many } from "data-modeling";
 
 export interface EntitySelectors<TEntityTypeMap extends EntityTypeMap, TModel> {
-    readonly getIds: Selector<EntityStateMap<TEntityTypeMap>, ReadonlyArray<string>>;
+    readonly getIds: Selector<EntityStateMap<TEntityTypeMap>, Many<string>>;
     readonly getEntities: Selector<EntityStateMap<TEntityTypeMap>, KVS<TModel>>;
-    readonly getAll: Selector<EntityStateMap<TEntityTypeMap>, ReadonlyArray<TModel>>;
+    readonly getAll: Selector<EntityStateMap<TEntityTypeMap>, Many<TModel>>;
     readonly getFirstSelected: Selector<EntityStateMap<TEntityTypeMap>, TModel>;
     readonly isEntitySelected: Selector<EntityStateMap<TEntityTypeMap>, boolean>;
 }
